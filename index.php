63e43547ffc8c6e13a8e71150f4b54d63a1fe9af
elemikedev <miketorresdj1@hotmail.com>
2023-02-16 15:08:17 -0500

    segundo commit

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/swiper@9.0.5/swiper-bundle.min.css" rel="stylesheet">
+    <title>San Bartolomé - San Bartolomé</title>
+</head>
+<body>
+    <nav>
+      <div class="logo">San Bartolomé</div>
+      <input type="checkbox" id="click">
+      <label for="click" class="menu-btn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <ul>
+        <li><a class="active" href="#">Inicio</a></li>
+        <li><a href="#">Admisiones</a></li>
+        <li><a href="#">Recorrido</a></li>
+        <li><a href="#">Donaciones</a></li>
+        <li><a href="#">Contactanos</a></li>
+      </ul>
+    </nav>
+    <section class="content swiper mySwiper">
+            <div class="wrapper swiper-wrapper">
+                <div class="slide swiper-slide">
+                    <img src="https://images.unsplash.com/photo-1588072432836-e10032774350?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" alt="" class="image"> 
+                    <div class="image-date">
+                        <span class="text">418 Años formando ciudadanos para un mejor país</span>
+                        <h2>Colegio Mayor <br/>
+                            San Bartolomé 
+                        </h2>
+                        <a href="#" class="button">Ver más...</a>
+                    </div>  
+                </div>
+
+                <div class="slide swiper-slide">
+                    <img src="https://images.unsplash.com/photo-1570616969692-54d6ba3d0397?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHNjaG9vbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" class="image"> 
+                    <div class="image-date">
+                        <span class="text">418 Años formando ciudadanos para un mejor país</span>
+                        <h2>Colegio Mayor <br/>
+                            San Bartolomé 
+                        </h2>
+                        <a href="#" class="button">Ver más...</a>
+                    </div>  
+                </div>
+
+                <div class="slide swiper-slide">
+                    <img src="https://images.unsplash.com/photo-1498243691581-b145c3f54a5a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" class="image"> 
+                    <div class="image-date">
+                        <span class="text">418 Años formando ciudadanos para un mejor país</span>
+                        <h2>Colegio Mayor <br/>
+                            San Bartolomé 
+                        </h2>
+                        <a href="#" class="button">Ver más...</a>
+                    </div> 
+                </div>
+            </div>
+        <div class="swiper-button-next nav-btn"></div>
+        <div class="swiper-button-prev nav-btn"></div>
+        <div class="swiper-pagination"></div>
+    </section>
+
+    <section class="section-cards">
+        <div class="section-cards_card card-blue">
+            <div class="section-cards-card_container">
+                <img src="https://scontent-bog1-1.xx.fbcdn.net/v/t1.6435-9/152342512_114600577337011_3712967971533023634_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=n6U1i50nWwwAX-3KLks&_nc_ht=scontent-bog1-1.xx&oh=00_AfAUG06zqVViSxjpmXQ67BdvqXlG6E04QaPh7zwdr0wZ8g&oe=6415DE67" alt="Una familia">
+            </div>
+            <div class="section-cards-card_details">
+                <h3>Colegio</h3>
+            </div>
+        </div>
+        <div class="section-cards_card card-red">
+            <div class="section-cards-card_container">
+                <img src="https://scontent-bog1-1.xx.fbcdn.net/v/t1.6435-9/152342512_114600577337011_3712967971533023634_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=n6U1i50nWwwAX-3KLks&_nc_ht=scontent-bog1-1.xx&oh=00_AfAUG06zqVViSxjpmXQ67BdvqXlG6E04QaPh7zwdr0wZ8g&oe=6415DE67" alt="Una familia">
+            </div>
+            <div class="section-cards-card_details">
+                <h3>Estudiantes</h3>
+            </div>
+        </div>
+        <div class="section-cards_card card-blue">
+            <div class="section-cards-card_container">
+                <img src="https://scontent-bog1-1.xx.fbcdn.net/v/t1.6435-9/152342512_114600577337011_3712967971533023634_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=n6U1i50nWwwAX-3KLks&_nc_ht=scontent-bog1-1.xx&oh=00_AfAUG06zqVViSxjpmXQ67BdvqXlG6E04QaPh7zwdr0wZ8g&oe=6415DE67" alt="Una familia">
+            </div>
+            <div class="section-cards-card_details">
+                <h3>Padres de Familia</h3>
+            </div>
+        </div>
+        <div class="section-cards_card card-red">
+            <div class="section-cards-card_container">
+                <img src="https://scontent-bog1-1.xx.fbcdn.net/v/t1.6435-9/152342512_114600577337011_3712967971533023634_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=n6U1i50nWwwAX-3KLks&_nc_ht=scontent-bog1-1.xx&oh=00_AfAUG06zqVViSxjpmXQ67BdvqXlG6E04QaPh7zwdr0wZ8g&oe=6415DE67" alt="Una familia">
+            </div>
+            <div class="section-cards-card_details">
+                <h3>Exalumnos</h3>
+            </div>
+        </div>
+        <div class="section-cards_card card-blue">
+            <div class="section-cards-card_container">
+                <img src="https://scontent-bog1-1.xx.fbcdn.net/v/t1.6435-9/152342512_114600577337011_3712967971533023634_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=n6U1i50nWwwAX-3KLks&_nc_ht=scontent-bog1-1.xx&oh=00_AfAUG06zqVViSxjpmXQ67BdvqXlG6E04QaPh7zwdr0wZ8g&oe=6415DE67" alt="Una familia">
+            </div>
+            <div class="section-cards-card_details">
+                <h3>Profesores y administradores</h3>
+            </div>
+        </div>
+    </section>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9.0.5/swiper-bundle.min.js">
+    </script>
+    <script>
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  </script>
+  </body>
+</html>
