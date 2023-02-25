d66936a413a6e076397ba690a135299f86563be2
elemikedev <miketorresdj1@hotmail.com>
2023-02-25 12:26:46 -0500

    Nuevo comienzo

--- index.php
+++ index.php
@@ -4,27 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-    <link rel="stylesheet" href="style.css">
-    <title>San Bartolomé - San Bartolomé</title>
+    <title>Document</title>
 </head>
 <body>
-    <nav>
-        <div class="logo">San Bartolomé</div>
-        <input type="checkbox" id="click">
-        <label for="click" class="menu-btn">
-            <i class="fas fa-bars"></i>
-        </label>
-        <ul>
-            <li><a class="active" href="#">Inicio</a></li>
-            <li><a href="#">Admisiones</a></li>
-            <li><a href="#">Recorrido</a></li>
-            <li><a href="#">Donaciones</a></li>
-            <li><a href="#">Contactanos</a></li>
-        </ul>
-    </nav>
+    
 </body>
 </html>
