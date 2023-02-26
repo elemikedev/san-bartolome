845731e5d73594f53b0451798c8d687c9e659ac8
elemikedev <miketorresdj1@hotmail.com>
2023-02-26 07:53:20 -0500

    Responsive

--- index.php
+++ index.php
@@ -4,35 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <!-- ===== Boxicons CSS ===== -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <title>Document</title>
 </head>
 <body>
-    <div class="top-bar">
-        <span><ion-icon name="call-outline"></ion-icon>9191990. ext 2211</span>
+        <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo"><a class="a-logo" href="">Colegio Mayor San Bartolomé</a></label>
         <ul>
-            <li><a href=""><ion-icon name="logo-facebook"></ion-icon></a></li>
-            <li><a href=""><ion-icon name="logo-twitter"></ion-icon></a></li>
-            <li><a href=""><ion-icon name="logo-instagram"></ion-icon></a></li>
+            <li><a class="active-1" href="#">Colegio</a></li>
+            <li><a class="active-2" href="#">Estudiantes</a></li>
+            <li><a class="active-3" href="#">Padres de familia</a></li>
+            <li><a class="active-4" href="#">Profesores y administrativos</a></li>
         </ul>
-    </div>
+        </nav>
 
-    <nav>
-        <div class="logo">
-            <a href=""><img src="" alt="logo"></a>
-        </div>
-        <div class="toggle">
-            <a href="#"><ion-icon name="menu-outline"></ion-icon></a>
-        </div>
-        <ul class="menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Home</a></li>
-        </ul>
-    </nav>
 
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>    
-</body>
+        <!-- <div class="pimg1">
+            <div class="ptext">
+                <span class="border">
+                    San bartolomé
+                </span>
+            </div>
+        </div> -->
+    </body>
 </html>
